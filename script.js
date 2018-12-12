f4eb643888fd4391391b476c582725403f7e86d3
COR Students <corstudents@Matts-MacBook-Pro-108.local>
2018-12-12 18:37:02 -0500

    initialized product

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    function calculate() {
+        let price = [$('#price').val()]
+let total =  $('#price').val().sum()
+    
+//   $("submitButton").click(function(){
+    // alert("The paragraph was clicked.");
+// });
+
+$("submitButton").submit(function(){
+    // alert("The paragraph was clicked.");
+});
+
+    } calculate()
+
+});
